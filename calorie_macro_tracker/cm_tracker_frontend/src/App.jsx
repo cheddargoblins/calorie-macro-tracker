39215f09b2e6d95ad0ee2631b187b4c363a0350f
daleherman03 <daherman66@yahoo.com>
2023-05-04 15:58:11 -0500

    added name field to App_User model

--- calorie_macro_tracker/cm_tracker_frontend/src/App.jsx
+++ calorie_macro_tracker/cm_tracker_frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { createContext, useEffect, useState } from "react";
 import "./App.css";
 import { currUser } from "./utilities";
 import { getToken } from "./components/CSRF";
+import { NavBar } from "./components/NavBar";
+import { Outlet } from "react-router-dom";
 
 export const UserContext = createContext([]);
 
