a473e7a178e5af8d63662f880075ab308e451060
brandonyork14 <brandon.york@yahoo.com>
2023-05-15 17:09:31 -0500

    homepage styled

--- calorie_macro_tracker/cm_tracker_frontend/src/App.jsx
+++ calorie_macro_tracker/cm_tracker_frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { createContext, useEffect, useState } from "react";
 import "./App.css";
 import { currUser } from "./utilities";
 import { getToken } from "./components/CSRF";
-import { NavBar } from "./components/NavBar";
+import { NavBar } from "./pages/NavBar";
 import { Outlet } from "react-router-dom";
 
 export const UserContext = createContext([]);
@@ -22,7 +22,7 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-
+      
       <UserContext.Provider value={{ user, setUser }}>
         <Outlet />
       </UserContext.Provider>
